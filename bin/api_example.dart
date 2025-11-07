985e8afc78555f49a1432a2f16f82f15350c7892
Jose Rafael <jbaeza_23@alu.uabcs.mx>
2025-11-07 00:17:51 -0700

    se actualizo la api

--- bin/api_example.dart
+++ bin/api_example.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
@@ -5,14 +6,60 @@ import 'package:shelf_router/shelf_router.dart';
 void main() async {
   final router = Router();
 
-  router.get('/api', (Request request){
+  final reports = [
+    {
+      'user': 'Juan Pérez',
+      'location': 'chiringuito chatarra ',
+      'image': '🌆',
+      'description': 'accidente de trafico',
+      'time': 'Hace 1 hora',
+    },
+    {
+      'user': 'Victoria seiko',
+      'location': 'Parque mangos',
+      'image': '🌳',
+      'description': 'mucho trafico',
+      'time': 'Hace 10 horas',
+    },
+    {
+      'user': 'Luis Manuel Perpuly Jordan',
+      'location': 'calle malandros',
+      'image': '🚧',
+      'description': 'poste caido',
+      'time': 'Hace 19 horas',
+    },
+    {
+      'user': 'Armando Paredes',
+      'location': 'calle argentina',
+      'image': '🚦',
+      'description': 'robo',
+      'time': 'Hace 8 horas',
+    },
+  ];
+
+  final users = [
+    {"name": "Luis", "age": 35, "phone": "612271564"},
+    {"name": "rafa", "age": 20, "phone": "6122776219"}
+  ];
+
+  // Endpoints
+  router.get('/hello', (Request request) {
     return Response.ok('Hola Mundo');
   });
 
-  final handler = const Pipeline()
-    .addMiddleware(logRequests())
-    .addHandler(router);
+  router.get('/reports', (Request request) {
+    return Response.ok(jsonEncode(reports),
+        headers: {'Content-Type': 'application/json'});
+  });
+
+  router.get('/users', (Request request) {
+    return Response.ok(jsonEncode(users),
+        headers: {'Content-Type': 'application/json'});
+  });
+
+  final handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(router);
 
   await io.serve(handler, 'localhost', 8080);
-  print('El servidor esta corriendo ene l puerto 8080');
+  print('Servidor corriendo en el puerto 8080');
 }
